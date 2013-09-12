2b54df77cbd0f6dbc88c96a5570c26118ca11d60
IEVin <i.e.vin@ya.ru>
2013-09-12 18:54:37 +0400

    Add test project

---
+++ src/TestCore/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace TestCore
+{
+	class Program
+	{
+		public static void Main( string[] args )
+		{
+			
+		}
+	}
+}
---
+++ src/TestCore/TestModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel;
+
+namespace TestCore
+{
+	public class TestModel : INotifyPropertyChanged
+	{
+		public event PropertyChangedEventHandler PropertyChanged;
+
+		public virtual string Name { get; set; }
+
+		public virtual int Age { get; set; }
+	}
+}
