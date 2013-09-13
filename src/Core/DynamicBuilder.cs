5b481ee008c6d3107bf209d51c5f5c4977c335a2
IEVin <i.e.vin@ya.ru>
2013-09-13 10:29:57 +0400

    Add DynamicBuilder

---
+++ src/Core/DynamicBuilder.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace NotifyAutoImplementer.Core
+{
+	public static class DynamicBuilder
+	{
+		public static T CreateInstanceProxy<T>()
+		{
+			return (T)CreateProxy( typeof(T) );
+		}
+
+		static object CreateProxy( Type type )
+		{
+			// stub
+			return Activator.CreateInstance( type );
+		}
+	}
+}
