01bd97f4da951e05f4066056904f607aa7866c12
IEVin <i.e.vin@ya.ru>
2013-09-13 19:47:26 +0400

    Add AssemblyBuilder

--- src/Core/DynamicBuilder.cs
+++ src/Core/DynamicBuilder.cs
@@ -1,9 +1,16 @@
 ﻿using System;
+using System.Reflection;
+using System.Reflection.Emit;
 
 namespace NotifyAutoImplementer.Core
 {
 	public static class DynamicBuilder
 	{
+		const string AssemblyBuilderName = "DynamicAssembly_871";
+
+		static Lazy<AssemblyBuilder> s_assemblyBuilder = new Lazy<AssemblyBuilder>(
+			() => AppDomain.CurrentDomain.DefineDynamicAssembly( new AssemblyName( AssemblyBuilderName ), AssemblyBuilderAccess.Run ) );
+
 		public static T CreateInstanceProxy<T>()
 		{
 			return (T)CreateProxy( typeof(T) );
