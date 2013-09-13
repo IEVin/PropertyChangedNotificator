943d8c032b0a042401775df5b9cd11b0ac4f1ef8
IEVin <i.e.vin@ya.ru>
2013-09-13 10:58:37 +0400

    Add simple test

--- src/TestCore/Program.cs
+++ src/TestCore/Program.cs
@@ -1,10 +1,31 @@
-﻿namespace TestCore
+﻿using System;
+using NotifyAutoImplementer.Core;
+
+namespace NotifyAutoImplementer.TestCore
 {
 	class Program
 	{
 		public static void Main( string[] args )
 		{
-			
+			var model = DynamicBuilder.CreateInstanceProxy<TestModel>();
+
+			var ageChangeCount = 0;
+			var nameChangeCount = 0;
+
+			model.PropertyChanged += ( o, e ) =>
+				{
+					if ( e.PropertyName == "Age" )
+						ageChangeCount++;
+					if ( e.PropertyName == "Name" )
+						nameChangeCount++;
+				};
+
+			model.Age = 1;
+			model.Name = "Test";
+			model.Age++;
+
+			Console.WriteLine( "Age change count = {0} (must 2)", ageChangeCount );
+			Console.WriteLine( "Name change count = {0} (must 1)", nameChangeCount );
 		}
 	}
 }
