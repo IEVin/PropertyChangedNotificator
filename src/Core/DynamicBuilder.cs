3eb2224e6e3e2a2c68f0a1f7b032027f31113523
IEVin <i.e.vin@ya.ru>
2013-09-24 01:02:28 +0400

    Move DynamicHelper to new file

--- src/Core/DynamicBuilder.cs
+++ src/Core/DynamicBuilder.cs
@@ -114,39 +114,4 @@ namespace NotifyAutoImplementer.Core
                             .DefineDynamicModule(assemblyName.Name);
         }
     }
-
-    public static class DynamicHelper
-    {
-        internal static MethodInfo GetRaise()
-        {
-            return typeof(NotifyPropertyObject)
-                .GetMethod("RaisePropertyChanged", BindingFlags.Instance | BindingFlags.NonPublic);
-        }
-
-        internal static MethodInfo GetEquals(Type type)
-        {
-            var method = type.IsClass ? "EqualsRef" : "EqualsVal";
-
-            return typeof(DynamicHelper)
-                .GetMethod(method, BindingFlags.Static | BindingFlags.Public)
-                .MakeGenericMethod(type);
-        }
-
-        public static bool EqualsRef<T>(T a, T b)
-            where T : class
-        {
-            return a != null
-                       ? a.Equals(b)
-                       : b == null;
-        }
-
-        public static bool EqualsVal<T>(T a, T b)
-            where T : struct
-        {
-            var eq = a as IEquatable<T>;
-            return eq != null
-                       ? eq.Equals(b)
-                       : a.Equals(b);
-        }
-    }
 }
---
+++ src/Core/DynamicHelper.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Reflection;
+
+namespace NotifyAutoImplementer.Core
+{
+    public static class DynamicHelper
+    {
+        internal static MethodInfo GetRaise()
+        {
+            return typeof(NotifyPropertyObject)
+                .GetMethod("RaisePropertyChanged", BindingFlags.Instance | BindingFlags.NonPublic);
+        }
+
+        internal static MethodInfo GetEquals(Type type)
+        {
+            var method = type.IsClass ? "EqualsRef" : "EqualsVal";
+
+            return typeof(DynamicHelper)
+                .GetMethod(method, BindingFlags.Static | BindingFlags.Public)
+                .MakeGenericMethod(type);
+        }
+
+        public static bool EqualsRef<T>(T a, T b)
+            where T : class
+        {
+            return a != null
+                       ? a.Equals(b)
+                       : b == null;
+        }
+
+        public static bool EqualsVal<T>(T a, T b)
+            where T : struct
+        {
+            var eq = a as IEquatable<T>;
+            return eq != null
+                       ? eq.Equals(b)
+                       : a.Equals(b);
+        }
+    }
+}
